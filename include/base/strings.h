2a39fe11bce25cbf04a99ec71f05e6c1a0e7c983
Tadeas Maca <maca.tadeas@gmail.com>
2025-01-31 15:21:39 +0100

    strings change

--- include/base/strings.h
+++ include/base/strings.h
@@ -1,16 +1,43 @@
 #ifndef STRINGS_H
 #define STRINGS_H
 
+#include "base/mem.h"
 #include "base/types.h"
 
 typedef struct {
-    u64 length;
-    u8 *c_str;
-} String;
+    u64 count;
+    u8 *str;
+} String8;
 
-void string_init(String* str, u8 *c_str);
-void string_init_length(String* str, u8 *c_str, u64 length);
+typedef struct {
+    u64 count;
+    u8 *str;
+} String8View;
 
-String substring(String* str, u64 start, u64 length);
+typedef struct {
+    m_Arena arena;
+    u64 count;
+    String8View *items;
+} String8ViewArray;
+
+String8 str8_alloc(m_Arena *arena, u64 count);
+void str8_init(String8 *string, u8* str, u64 count);
+String8 str8(u8* str, u64 count);
+String8 str8_cstr(u8* cstr);
+String8 str8_range(u8* start, u8* end);
+
+String8View str8_view(String8* str);
+
+String8View str8_substr(String8View* str, u64 start, u64 length);
+u8 str8_at(String8View* str);
 
 #endif
+
+
+
+
+
+
+
+
+
--- src/base/strings.c
+++ src/base/strings.c
@@ -1,29 +1,76 @@
 #include "base/strings.h"
+#include "base/defines.h"
+#include "base/mem.h"
+#include "base/ds.h"
 
-void
-string_init_length(String *str, u8 *c_str, u64 length) {
-    str->length = length;
-    str->c_str = c_str;
+String8
+str8_alloc(m_Arena *arena, u64 count) {
+    u8* str = m_arena_push(arena, sizeof(*str) * count);
+    String8 result = (String8){.str = str, .count = count};
+    return result;
 }
 
-void
-string_init(String *str, u8 *c_str) {
-    u64 len = 0;
-    while(c_str[len] != null) {
-        len++;
+String8
+str8(u8 *str, u64 count) {
+    return (String8){.str = str, .count = count};
+}
+
+String8
+str8_cstr(u8 *cstr) {
+    u64 count = 0;
+    String8 str;
+    str.str = cstr;
+    
+    while(cstr[count]) {
+        count++;
     }
 
-    str->c_str = c_str;
-    str->length = len;
+    return str;
 }
 
-String
-substring(String *str, u64 start, u64 length) {
-    String substr;
-    substr.c_str = str->c_str;
-    substr.c_str += start;
-    substr.length = length;
-    return substr;
+String8
+str8_range(u8 *start, u8 *end) {
+    String8 str;
+    str.count = end - start;
+    str.str = start;
+    return str;
 }
 
+String8View
+str8_view(String8 *str) {
+    return (String8View){.str = str->str, .count = str->count};
+}
 
+String8View
+str8_substr(String8View *str, u64 start, u64 count) {
+    return (String8View){.str = str->str + start, .count = count};
+}
+
+String8ViewArray
+str8_split(String8View *view, u8 splitter) {
+    struct SplitPos {
+        int start;
+        int end;
+    } splitPos;
+
+
+    String8ViewArray array;
+    DArrayInit(&array);
+    
+
+    struct splitPos = (SplitPos){.start = 0, .end = 0};
+    
+    struct {
+        m_Arena arena;
+        u64 count;
+
+    }
+
+
+    for(i = 0; i < view->count; i++) {
+        if(view->str[i] == splitter) {
+        }
+    }
+
+    return array;
+}
