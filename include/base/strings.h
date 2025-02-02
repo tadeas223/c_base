4ddab1aa33cbbdc845cac8a6edd55843cfb9b0a3
Tadeas Maca <maca.tadeas@gmail.com>
2025-02-02 13:40:25 +0100

    I can't View

--- include/base/strings.h
+++ include/base/strings.h
@@ -3,7 +3,6 @@
 
 #include "base/mem.h"
 #include "base/types.h"
-#include "base/defines.h"
 
 #define Str8Lit(c_str) str8_cstr_count(c_str, sizeof(c_str) - 1)
 
@@ -12,44 +11,39 @@ typedef struct {
     u8 *str;
 } String8;
 
-typedef struct {
-    u64 count;
-    u8 *str;
-} String8View;
-
-typedef struct String8ViewNode {
-    struct String8ViewNode *next;
-    struct String8ViewNode *prev;
-    String8View str;
-} String8ViewNode;
+typedef struct String8Node {
+    struct String8Node *next;
+    struct String8Node *prev;
+    String8 str;
+} String8Node;
 
 typedef struct {
-    String8ViewNode *head;
-    String8ViewNode *tail;
+    String8Node *head;
+    String8Node *tail;
     u64 node_count;
     u64 str_count;
-} String8ViewList;
+} String8List;
 
 String8 str8(u8* str, u64 count);
 String8 str8_range(u8* start, u8* end);
 String8 str8_alloc(m_Arena *arena, u64 count);
 
-String8View str8_view(String8 str);
-String8View str8_cstr(const char* cstr);
-String8View str8_cstr_count(const char* cstr, u64 count);
+String8 str8_view(String8 str);
+String8 str8_cstr(const char* cstr);
+String8 str8_cstr_count(const char* cstr, u64 count);
+
+String8 substr(String8 string, u64 start, u64 count);
+
+void str8_list_init(String8List *list);
+void str8_list_push(m_Arena *arena, String8List *list, String8 string);
+String8 str8_list_pop(String8List *list);
+
+String8List str8_split(m_Arena *arena, String8 string, u8 splitter);
+String8 str8_join(m_Arena *arena, String8List *list);
 
 void str8_print(String8 str);
-void str8_view_print(String8View view);
 
-u8 str8_at(String8View view, u64 pos);
-
-void str8_list_init(String8ViewList *list);
-void str8_list_push(m_Arena *arena, String8ViewList *list, String8View view);
-String8View str8_list_pop(String8ViewList *list);
-
-String8View str8_substr(String8View view, u64 start, u64 length);
-String8ViewList* str8_split(m_Arena *arena, String8View view, u8 splitter);
-String8 str8_join(m_Arena *arena, String8ViewList *list);
+u8 str8_at(String8 view, u64 pos);
 
 bool is_number(u8 c);
 bool is_white_space(u8 c);
--- src/base/strings.c
+++ src/base/strings.c
@@ -15,10 +15,10 @@ str8(u8 *str, u64 count) {
     return (String8){.str = str, .count = count};
 }
 
-String8View
+String8
 str8_cstr(const char *cstr) {
     u64 count = 0;
-    String8View str;
+    String8 str;
     str.str = (u8*)cstr;
     
     while(cstr[count] != null) {
@@ -30,50 +30,39 @@ str8_cstr(const char *cstr) {
     return str;
 }
 
-String8View
+String8
 str8_cstr_count(const char *cstr, u64 count) {
-    String8View str;
-    str.str = (u8*)cstr;
-    str.count = count;
-    return str;
+    String8 string;
+    string.str = (u8*)cstr;
+    string.count = count;
+    return string;
 }
 
 String8
 str8_range(u8 *start, u8 *end) {
-    String8 str;
-    str.count = end - start;
-    str.str = start;
-    return str;
+    String8 string;
+    string.count = end - start;
+    string.str = start;
+    return string;
 }
 
 void
-str8_print(String8 str) {
+str8_print(String8 string) {
     u64 i;
-    for(i = 0; i < str.count; i++) {
-        printf("%c", str.str[i]);
+    for(i = 0; i < string.count; i++) {
+        printf("%c", string.str[i]);
     }
 }
 
+String8
+str8_substr(String8 string, u64 start, u64 count) {
+    return (String8){.str = string.str + start, .count = count};
+}
+
+
+/* same as list = {0} */
 void
-str8_view_print(String8View view) {
-    u64 i;
-    for(i = 0; i < view.count; i++) {
-        printf("%c", view.str[i]);
-    }
-}
-
-String8View
-str8_view(String8 str) {
-    return (String8View){.str = str.str, .count = str.count};
-}
-
-String8View
-str8_substr(String8View view, u64 start, u64 count) {
-    return (String8View){.str = view.str + start, .count = count};
-}
-
-void
-str8_list_init(String8ViewList *list) {
+str8_list_init(String8List *list) {
     list->head = null;
     list->tail = null;
     list->node_count = 0;
@@ -81,9 +70,9 @@ str8_list_init(String8ViewList *list) {
 }
 
 void
-str8_list_push(m_Arena *arena, String8ViewList *list, String8View view) {
-    String8ViewNode *node = m_arena_push(arena, sizeof(String8ViewNode));
-    node->str = view;
+str8_list_push(m_Arena *arena, String8List *list, String8 string) {
+    String8Node *node = m_arena_push(arena, sizeof(*node));
+    node->str = string;
     node->next = null;
     node->prev = null;
 
@@ -99,47 +88,47 @@ str8_list_push(m_Arena *arena, String8ViewList *list, String8View view) {
         list->tail = node;
     }
     list->node_count++;
-    list->str_count += view.count;
+    list->str_count += string.count;
 }
 
-String8View
-str8_list_pop(String8ViewList *list) {
-    String8View view = list->tail->str;
+String8
+str8_list_pop(String8List *list) {
+    String8 string = list->tail->str;
     list->tail = list->tail->prev;
     list->node_count--;
-    list->str_count -= view.count;
-    return view;
+    list->str_count -= string.count;
+    return string;
 }
 
 
 
-String8ViewList*
-str8_split(m_Arena *arena, String8View view, u8 splitter) {
+String8List
+str8_split(m_Arena *arena, String8 string, u8 splitter) {
     u64 splitStart = 0;
     u64 i = 0;
-    String8ViewList *list = m_arena_push(arena, sizeof(*list));
-    str8_list_init(list);
+    String8List list;
+    str8_list_init(&list);
 
-    for(i = 0; i < view.count; i++) {
-        if(view.str[i] == splitter) {
-            String8View splitView = str8_substr(view, splitStart, i - splitStart);
-            str8_list_push(arena, list, splitView);
+    for(i = 0; i < string.count; i++) {
+        if(string.str[i] == splitter) {
+            String8 splitString = str8_substr(string, splitStart, i - splitStart);
+            str8_list_push(arena, &list, splitString);
             splitStart = i + 1; /* skip the splitter */
         }
     }
    
-    String8View splitView = str8_substr(view, splitStart, i - splitStart);
-    str8_list_push(arena, list, splitView);
+    String8 splitString = str8_substr(string, splitStart, i - splitStart);
+    str8_list_push(arena, &list, splitString);
 
     return list;
 }
 
 String8
-str8_join(m_Arena *arena, String8ViewList *list) {
+str8_join(m_Arena *arena, String8List *list) {
     String8 str = str8_alloc(arena, list->str_count);
 
     u64 counter = 0;
-    String8ViewNode *node = list->head;
+    String8Node *node = list->head;
     for(node = list->head; node != null; node = node->next) {
         m_copy(str.str + counter, node->str.str, node->str.count); 
         counter += node->str.count;
@@ -149,7 +138,7 @@ str8_join(m_Arena *arena, String8ViewList *list) {
 }
 
 u8
-str8_at(String8View view, u64 pos) {
-    if(pos >= view.count) return null;
-    return view.str[pos];
+str8_at(String8 string, u64 pos) {
+    if(pos >= string.count) return null;
+    return string.str[pos];
 }
